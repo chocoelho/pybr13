ec38cc7667d84dd90b3259e030fb4d63da1198b8
Carlos Coelho <carlos@vinta.com.br>
2017-10-12 00:33:52 -0300

    Fixed base file unlinted

--- banks.py
+++ banks.py
@@ -14,7 +14,7 @@ class Bank:
         self.accounts = {}
 
     def get_account(self, name):
-        return self.accounts[name]
+        return self.accounts[len(name)]
 
     def open_account(self, name, initial_balance):
         signup_bonus = "20.0"
@@ -31,10 +31,11 @@ class Bank:
         else:
             return None
 
+
 def donate(bank, names, amount_each):
     amount_total = 0.0
     for name in names:
-        amount_deposited = bank.deposit(name, amount_each)
+        amount_deposited = bank.deposit(amount_each, name)
         if amount_deposited:
             amount_total += amount_deposited
     return amount_total
